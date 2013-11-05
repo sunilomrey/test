415aa94d52cf2ead165abf330f59f1d4a16d0db7
Paras-00002 <paras-00002@mail.localhost>
2013-11-05 18:08:51 +0530

    my first commit

---
+++ page02.php
@@ -0,0 +1,25 @@
+<style>
+    @import 'style.css';
+</style>
+<?php
+$search = "mumbai";
+$url = 'http://en.wikipedia.org/w/api.php?action=parse&page='.$search.'&format=json&prop=text&section=0';
+$ch = curl_init($url);
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt ($ch, CURLOPT_USERAGENT, "TestScript");
+$c = curl_exec($ch);
+$json = json_decode($c);
+$content = $json->{'parse'}->{'text'}->{'*'}; 
+
+$pattern = '#<p>(.*)</p>#Us'; 
+if(preg_match($pattern, $content, $matches))
+{    
+   $para = preg_replace('~\/.*?\/~','',strip_tags($matches[1]));
+   $para = preg_replace('~\[.*?\]~','',$para);
+   $para = preg_replace('~\{.*?\}~','',$para);
+   $para = preg_replace('~\(.*?\)~','',$para);
+   $para = preg_replace('~\).*?~','',$para);
+}
+print $para;
+echo "<br><br><br><br><br>";
+?>
